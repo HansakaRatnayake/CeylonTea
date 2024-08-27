719e3a3d8c5b0be308eed7d10762dd3d34f77f74
hansakarathnayake <hndk2002@gmail.com>
2024-08-27 22:40:46 +0530

    initial commit

---
+++ Server/cylontea/src/main/java/com/products/cylontea/CylonteaApplication.java
@@ -0,0 +1,13 @@
+package com.products.cylontea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CylonteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CylonteaApplication.class, args);
+	}
+
+}
